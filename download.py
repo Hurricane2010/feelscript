2c48732cc32b6227d294e812843c13c1b21b2d02
Hurricane2010 <rishu0129@gmail.com>
2024-12-24 20:42:29 -0800

    Tried readding nltk data

--- download.py
+++ download.py
@@ -12,10 +12,12 @@ if not os.path.exists(nltk_data_dir):
 nltk.data.path.append(nltk_data_dir)
 
 # Now download the resources to the specified directory
-nltk.download('punkt_tab', download_dir=nltk_data_dir)
+# nltk.download('punkt_tab', download_dir=nltk_data_dir)
+# nltk.download('punkt', download_dir=nltk_data_dir)
 # nltk.download('vader_lexicon', download_dir=nltk_data_dir)
 # nltk.download('wordnet', download_dir=nltk_data_dir)
 # nltk.download('averaged_perceptron_tagger', download_dir=nltk_data_dir)
 # nltk.download('stopwords', download_dir=nltk_data_dir)
+nltk.download('averaged_perceptron_tagger_eng', download_dir=nltk_data_dir)
 
 print("NLTK resources downloaded to the specified directory.")
